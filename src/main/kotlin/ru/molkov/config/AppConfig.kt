d292eb9823d8778eb7474a83f1a10009c2721201
amolkov <alexandr.molkov@gmail.com>
2017-03-12 16:59:55 +0300

    Changed configuration

--- src/main/kotlin/ru/molkov/config/AppConfig.kt
+++ src/main/kotlin/ru/molkov/config/AppConfig.kt
@@ -1,5 +1,6 @@
 package ru.molkov.config
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.PropertySource
@@ -8,4 +9,8 @@ import ru.molkov.Application
 @Configuration
 @PropertySource("classpath:application.properties")
 @ComponentScan(basePackageClasses = arrayOf(Application::class))
-open class AppConfig
+open class AppConfig {
+
+    @Value("\${nasaApiKey}")
+    lateinit var nasaApiKey: String
+}
--- src/main/kotlin/ru/molkov/service/NasaWebServiceImpl.kt
+++ src/main/kotlin/ru/molkov/service/NasaWebServiceImpl.kt
@@ -1,6 +1,7 @@
 package ru.molkov.service
 
 import org.springframework.stereotype.Service
+import ru.molkov.config.AppConfig
 import ru.molkov.entity.Apod
 import ru.molkov.repository.ApodRepository
 import ru.molkov.service.api.NasaApi
@@ -9,8 +10,8 @@ import ru.molkov.setting.asString
 import java.util.*
 
 @Service
-class NasaWebServiceImpl(val apodRepository: ApodRepository) : AbstractWebService(Constants.NASA_BASE_URL), NasaWebService {
-    private val NASA_API_KEY = "YOUR_API_KEY"
+class NasaWebServiceImpl(val apodRepository: ApodRepository, appConfig: AppConfig) : AbstractWebService(Constants.NASA_BASE_URL), NasaWebService {
+    private val NASA_API_KEY = appConfig.nasaApiKey
     private val nasaApi: NasaApi
 
     init {
